6156f549c5fffa4cc8eabf4c1ebf07b8942fe8ba
Temppa127 <temppa127@gmail.com>
2025-05-03 14:23:30 +0300

    dropdown menu time

--- JS/jquery2.js
+++ JS/jquery2.js
@@ -14,12 +14,28 @@ $(function(){
     })
 
 
-    $(window).scroll(function(){
+    $(window).on("scroll",function(){
         $(".fadeable").each(function(i,obj){
             console.log(getOffset(obj).top)
             if (getOffset(obj).top < 550) {$(this).fadeTo("slow", 1)}
         })
 
     })
+
+
+    let clickBool = false;
+    $(".dropbtn").on("click", function() {
+
+        if(!clickBool) { $(this).siblings().css({"display": "block"}); clickBool = true;}
+        else {$(this).siblings().css({"display": "none"}); clickBool = false;}
+    })
+
+    $(window).on("mousemove", function() {
+
+        console.log($(".dropdown:hover").length)
+        if($(".dropdown:hover").length == 0) {$(".dropbtn").siblings().css({"display": "none"}); clickBool = false;}
+
+    })
+
 
 })
